290e9946f695336a89e614533ef2b7f79d4031dd
Sanjana Kumbhar <120375747+SanjanaK1906@users.noreply.github.com>
2025-02-19 13:17:20 +0530

    Update and rename Java Date and Time to Java Date and Time.java

--- Hackerrank/Java Date and Time.java
+++ Hackerrank/Java Date and Time.java
@@ -1,4 +1,4 @@
-The Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
+/*The Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
 Sample Input
 
 08 05 2015
@@ -6,7 +6,7 @@ Sample Output
 
 WEDNESDAY
 ----------------------------------
-
+*/
 import java.io.*;
 import java.math.*;
 import java.security.*;
