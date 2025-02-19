e7accda04393d13e2f6a75841309e60297321461
Sanjana Kumbhar <120375747+SanjanaK1906@users.noreply.github.com>
2025-02-19 13:18:51 +0530

    Update and rename Java Loops II to Java Loops II.java

--- Hackerrank/Java Loops II.java
+++ Hackerrank/Java Loops II.java
@@ -1,4 +1,4 @@
-Sample Input
+/*Sample Input
 
 2
 0 2 10
@@ -8,7 +8,7 @@ Sample Output
 2 6 14 30 62 126 254 510 1022 2046
 8 14 26 50 98
 ------------------------------------------
-
+*/
 import java.util.*;
 import java.io.*;
 
