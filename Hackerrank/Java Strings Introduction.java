94f0085c02b640962c89025943bd11c03e5110dd
Sanjana Kumbhar <120375747+SanjanaK1906@users.noreply.github.com>
2025-02-21 18:55:57 +0530

    Update Java Strings Introduction.java

--- Hackerrank/Java Strings Introduction.java
+++ Hackerrank/Java Strings Introduction.java
@@ -22,7 +22,6 @@
 //Hello Java
 //Explanation
 
-
 import java.io.*;
 import java.util.*;
 
@@ -36,12 +35,15 @@ public class Solution {
         /* Enter your code here. Print output to STDOUT. */
         int n = A.length()+B.length();
         System.out.println(n);
-        if(A.length()>B.length()){
+        if(A.compareTo(B)<=0){
             System.out.println("No");
+        }else{
+            System.out.println("Yes");
         }
         System.out.println(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
     }
 }
+
 
 
 
