6f07c55846bddd574b586022c370b8e17d2562c5
Sanjana Kumbhar <120375747+SanjanaK1906@users.noreply.github.com>
2025-02-22 00:33:31 +0530

    Update Java String Anagrams_best.java

--- Hackerrank/Java String Anagrams_best.java
+++ Hackerrank/Java String Anagrams_best.java
@@ -1,19 +1,4 @@
-/*
- palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
 
-Given a string , print Yes if it is a palindrome, print No otherwise.
-
-Constraints
-
-A will consist at most  50 lower case english letters.
-Sample Input
-
-madam
-Sample Output
-
-Yes
-
-*/
 import java.util.Scanner;
 
 public class Solution {
